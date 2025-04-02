358bbc11671de5ee2ad6bf8e36f9840625c341d6
felixstuart <148880497+felixstuart@users.noreply.github.com>
2025-04-02 09:04:11 -0400

    started work on printing 19 boards

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MyPlayer {
@@ -25,12 +26,14 @@ public class MyPlayer {
         row = 1;
         column = 1;
 
-        /***
-         * This code will run each time the "MyPlayer" button is pressed.
-         * Add your code to return the row and the column of the chip you want to take.
-         * You'll be returning a data type called Point which consists of two integers.
+        /*
+          This code will run each time the "MyPlayer" button is pressed.
+          Add your code to return the row and the column of the chip you want to take.
+          You'll be returning a data type called Point which consists of two integers.
          */
         toColumns();
+//        print all the different possible boards in one go
+        printBoards();
         Point myMove = new Point(row, column);
         System.out.println(Arrays.toString(columns));
         return myMove;
@@ -45,5 +48,31 @@ public class MyPlayer {
             }
         }
     }
+
+    public void printBoards() {
+        ArrayList<Integer> prettyBoard = new ArrayList<Integer>();
+        int[][] board = new int[3][3];
+        prettyBoard.add(3);
+        prettyBoard.add(3);
+        prettyBoard.add(3);
+
+        for (int[] i : board) Arrays.fill(i, 1);
+
+        System.out.println(prettyBoard);
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[0].length; x++) {
+//                remove everything right and above
+                board[x][y] = 0;
+//                prettify the board and print it
+                for (int[] i : board) {
+                    for (int j = 0; j < board[0].length; j++) {
+                        if (i[j] != 1) break;
+                        prettyBoard.set(j, prettyBoard.get(j)+1);
+                    }
+                }
+            }
+        }
+    }
+
 
 }
